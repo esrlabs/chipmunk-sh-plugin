5e6eafc99931518ca3bffadd84471fb8da27b309
DmitryAstafyev <dvastafyev@gmail.com>
2020-03-04 23:09:38 +0100

    [](feat) init

---
+++ render/src/lib/common/host.events.ts
@@ -0,0 +1,12 @@
+export enum EHostEvents {
+    ForkStarted = 'ForkStarted',
+    ForkClosed = 'ForkClosed',
+    SettingsUpdated = 'SettingsUpdated',
+}
+
+export enum EHostCommands {
+    command = 'command',
+    write = 'write',
+    stop = 'stop',
+    getSettings = 'getSettings',
+}
